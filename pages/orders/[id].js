21a7426b00731b07b3b04c51f65369d8157cec7c
Alif-07 <mokaddis.alif@gmail.com>
2022-02-10 00:46:46 +0600

    order part

--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -3,6 +3,13 @@ import React from 'react';
 import styles from '../../styles/Order.module.css';
 
 const Order = () => {
+	const status = 0;
+
+	const statusClass = (index) => {
+		if (index - status < 1) return styles.done;
+		if (index - status === 1) return styles.inProgress;
+		if (index - status > 1) return styles.undone;
+	};
 	return (
 		<div className={styles.container}>
 			<div className={styles.left}>
@@ -31,32 +38,56 @@ const Order = () => {
 					</table>
 				</div>
 				<div className={styles.row}>
-					<div className={styles.status}>
+					<div className={statusClass(0)}>
 						<Image src="/img/paid.png" width={30} height={30} alt="" />
 						<span>Payment</span>
 						<div className={styles.checkedIcon}>
-							<Image src="/img/checked.png" width={20} height={20} alt="" />
+							<Image
+								className={styles.checkedIcon}
+								src="/img/checked.png"
+								width={20}
+								height={20}
+								alt=""
+							/>
 						</div>
 					</div>
-					<div className={styles.status}>
+					<div className={statusClass(1)}>
 						<Image src="/img/bake.png" width={30} height={30} alt="" />
 						<span>Preparing</span>
 						<div className={styles.checkedIcon}>
-							<Image src="/img/checked.png" width={20} height={20} alt="" />
+							<Image
+								className={styles.checkedIcon}
+								src="/img/checked.png"
+								width={20}
+								height={20}
+								alt=""
+							/>
 						</div>
 					</div>
-					<div className={styles.status}>
+					<div className={statusClass(2)}>
 						<Image src="/img/bike.png" width={30} height={30} alt="" />
 						<span>On the way</span>
 						<div className={styles.checkedIcon}>
-							<Image src="/img/checked.png" width={20} height={20} alt="" />
+							<Image
+								className={styles.checkedIcon}
+								src="/img/checked.png"
+								width={20}
+								height={20}
+								alt=""
+							/>
 						</div>
 					</div>
-					<div className={styles.status}>
+					<div className={statusClass(3)}>
 						<Image src="/img/delivered.png" width={30} height={30} alt="" />
 						<span>Delivered</span>
 						<div className={styles.checkedIcon}>
-							<Image src="/img/checked.png" width={20} height={20} alt="" />
+							<Image
+								className={styles.checkedIcon}
+								src="/img/checked.png"
+								width={20}
+								height={20}
+								alt=""
+							/>
 						</div>
 					</div>
 				</div>
@@ -74,7 +105,7 @@ const Order = () => {
 						<b className={styles.totalTextTitle}>Total:</b>$79.60
 					</div>
 					<button disabled className={styles.button}>
-						Paid
+						PAID
 					</button>
 				</div>
 			</div>
